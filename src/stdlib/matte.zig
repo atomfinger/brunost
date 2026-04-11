b82f10f432ad59eccec4d6d6edb5c628bb7943a4
John Mikael Lindbakk <mikael.lindbakk@bspoke.no>
2026-04-11 18:16:23 +0200

    Adding random number function to stdlib

--- src/stdlib/matte.zig
+++ src/stdlib/matte.zig
@@ -10,6 +10,7 @@ pub fn make(alloc: std.mem.Allocator) EvalError!Value {
         .{ .name = "abs",  .value = .{ .builtin_fn = abs } },
         .{ .name = "maks", .value = .{ .builtin_fn = maks } },
         .{ .name = "min",  .value = .{ .builtin_fn = min } },
+        .{ .name = "tilfeldig", .value = .{ .builtin_fn = tilfeldig } },
     });
     return Value{ .module = members };
 }
@@ -32,4 +33,13 @@ fn min(args: []const Value, _: *Interpreter) EvalError!Value {
     const a = try args[0].as_int();
     const b = try args[1].as_int();
     return Value{ .integer = if (a < b) a else b };
+}
+
+fn tilfeldig(args: []const Value, _: *Interpreter) EvalError!Value {
+    if (args.len > 2) return EvalError.TypeError;
+    const minVal = if (args.len > 0) try args[0].as_int() else std.math.minInt(i64);
+    const maxVal = if (args.len > 1) try args[1].as_int() else std.math.maxInt(i64);
+    const rand = std.crypto.random;
+    const result = rand.intRangeAtMost(i64, minVal, maxVal);
+    return Value{ .integer = result };
 }
